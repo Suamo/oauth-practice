d8f73b7e9d68cd2c3f88b63367599e08bc7a6445
Anton Vyshnevskyi <forvao@gmail.com>
2020-12-12 16:19:22 +0200

    social authorization

--- social/src/main/java/com/example/social/SocialConnectApplication.java
+++ social/src/main/java/com/example/social/SocialConnectApplication.java
@@ -1,13 +1,13 @@
-package com.example.oauthpractice;
+package com.example.social;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OauthPracticeApplication {
+public class SocialConnectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OauthPracticeApplication.class, args);
+		SpringApplication.run(SocialConnectApplication.class, args);
 	}
 
 }
